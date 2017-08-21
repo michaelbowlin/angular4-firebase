6fd485bd42cc23c2e0d26e5c6546ef1a59eea9c2
Mike Bowlin <mike.bowlin@appliedpathways.com>
2017-08-20 20:33:28 -0600

    master/ng4-base: Working starter application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA7QkeebTMPs4DxgtsnYNy3v9K01g9ZvcY",
+  authDomain: "myapp-10b72.firebaseapp.com",
+  databaseURL: "https://myapp-10b72.firebaseio.com",
+  storageBucket: "myapp-10b72.appspot.com",
+  messagingSenderId: "44536681221"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    // HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
